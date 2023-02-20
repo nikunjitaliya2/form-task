8da1a7d0aac576857ed5a7efe2b0f48484781c2f
nikunj_gd <nikunj.italiya@ghanshyamdigital.com>
2023-02-20 16:49:51 +0530

    form task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-practice';
+  title = "form practice";
+  // userLogin(arg: any){
+  //   console.log(arg);
+  // }
+  // loginFrom = new FormGroup({
+  //   user:new FormControl('',[Validators.required,Validators.email]),
+  //   password:new FormControl('',[Validators.required,Validators.minLength(5)]),
+  // })
+  // loginUser(){
+  //   console.log(this.loginFrom.value);
+  // }
+  // get userValidator(){
+  //   return this.loginFrom.get('user');
+  // }
+  // get passwordValidation(){
+  //   return this.loginFrom.get('password');
+  // }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { RedElDirective } from './directive/red-el.directive';
+import { FormValidationComponent } from './form-validation/form-validation.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedElDirective,
+    FormValidationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
